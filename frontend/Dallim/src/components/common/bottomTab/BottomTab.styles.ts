80519fcfde3be383a09d0673a3ab545e07de2b9b
EUNSEONG LEE <pro30343069@gmail.com>
2023-10-16 13:07:43 +0900

    Feat: BottomTabNavigator

---
+++ frontend/Dallim/src/components/common/bottomTab/BottomTab.styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native';
+
+// 각 탭 아이콘의 스타일
+export const TabIcon = styled.Image`
+  width: 25px;
+  height: 25px;
+`;
+
---
+++ frontend/Dallim/src/components/common/bottomTab/BottomTab.tsx
@@ -0,0 +1,79 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import * as S from './BottomTab.styles';
+
+import Main from '../../../screens/main/Main';
+import Chart from '../../../screens/chart/Chart';
+import Social from '../../../screens/social/Social';
+import Edit from '../../../screens/edit/Edit';
+import Profile from '../../../screens/profile/Profile';
+
+const Tab = createBottomTabNavigator();
+
+function BottomTab() {
+    return (
+        <Tab.Navigator screenOptions={{ tabBarShowLabel: false }}>
+            <Tab.Screen
+                name="Chart"
+                component={Chart}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <S.TabIcon
+                            source={require('../../../assets/icons/chart-icon.png')}
+                            tintColor={focused ? 'blue' : 'gray'}
+                        />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Social"
+                component={Social}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <S.TabIcon
+                            source={require('../../../assets/icons/social-icon.png')}
+                            tintColor={focused ? 'blue' : 'gray'}
+                        />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Main"
+                component={Main}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <S.TabIcon
+                            source={require('../../../assets/icons/main-icon.png')}
+                            tintColor={focused ? 'blue' : 'gray'}
+                        />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Edit"
+                component={Edit}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <S.TabIcon
+                            source={require('../../../assets/icons/edit-icon.png')}
+                            tintColor={focused ? 'blue' : 'gray'}
+                        />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Profile"
+                component={Profile}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <S.TabIcon
+                            source={require('../../../assets/icons/profile-icon.png')}
+                            tintColor={focused ? 'blue' : 'gray'}
+                        />
+                    ),
+                }}
+            />
+        </Tab.Navigator>
+    )
+}
+
+export default BottomTab;

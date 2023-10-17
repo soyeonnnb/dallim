53153398260611f157323be565d6ae1462704864
GyuhoChoi <s4248942@gmail.com>
2023-10-18 02:20:49 +0900

    Comment : 주석 수정

--- frontend/Dallim/src/screens/Login/KakaoLogin.tsx
+++ frontend/Dallim/src/screens/Login/KakaoLogin.tsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 interface KakaoLoginProps {
-  navigation: any; // navigation prop 타입은 실제 사용하는 라이브러리에 따라 다를 수 있습니다.
+  navigation: any;
 }
 
 const KakaoLogin = ({navigation}: KakaoLoginProps) => {

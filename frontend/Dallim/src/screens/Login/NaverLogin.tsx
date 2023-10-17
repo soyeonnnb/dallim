13bf05fd4ef4ed7908c4dc3014847eb97d3cbcb1
GyuhoChoi <s4248942@gmail.com>
2023-10-18 02:56:20 +0900

    Feat : 로딩창 1차 구현

--- frontend/Dallim/src/screens/Login/NaverLogin.tsx
+++ frontend/Dallim/src/screens/Login/NaverLogin.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View} from 'react-native';
+import {View, ActivityIndicator} from 'react-native';
 import WebView from 'react-native-webview';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -9,22 +9,20 @@ interface NaverLoginProps {
 }
 
 const NaverLogin = ({navigation}: NaverLoginProps) => {
-  const [accessToken, setAccessToken] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const parseAuthCode = async (url: string) => {
-    const exp = 'code='; // URL에서 인가 코드 추출
+    const exp = 'code=';
     const startIndex = url.indexOf(exp);
     if (startIndex !== -1) {
-      const endIndex = url.indexOf('&', startIndex); // '&' 문자를 기준으로 파라미터 값을 추출합니다.
+      const endIndex = url.indexOf('&', startIndex);
       const authCode = url.substring(
         startIndex + exp.length,
         endIndex !== -1 ? endIndex : undefined,
       );
 
-      // authCode를 사용하여 로직을 수행하면 됩니다.
-      console.log('인가 코드: ' + authCode);
+      setIsLoading(true);
 
-      // 네이버 로그인 API로 액세스 토큰 요청
       await axios
         .post('http://10.0.2.2:8080/api/oauth2/code/naver', null, {
           params: {
@@ -34,10 +32,12 @@ const NaverLogin = ({navigation}: NaverLoginProps) => {
         .then(async res => {
           const accessToken = res.data.accessToken;
           await AsyncStorage.setItem('accessToken', accessToken);
-          console.log('Access Token: ', accessToken);
         })
         .catch(error => {
           console.error('Axios Error: ', error);
+        })
+        .finally(() => {
+          setIsLoading(false);
         });
       navigation.navigate('Main', {screen: 'Main'});
     }
@@ -45,31 +45,31 @@ const NaverLogin = ({navigation}: NaverLoginProps) => {
 
   return (
     <View style={{flex: 1}}>
-      <WebView
-        originWhitelist={['*']}
-        scalesPageToFit={false}
-        style={{marginTop: 30}}
-        source={{
-          uri: 'https://nid.naver.com/oauth2.0/authorize?client_id=U981wCCDuUbK6_3C3WJo&response_type=code&redirect_uri=http://localhost:8080/login/oauth2/code/naver', // 네이버 로그인 페이지 URL
-          headers: {
+      {isLoading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        <WebView
+          originWhitelist={['*']}
+          scalesPageToFit={false}
+          style={{marginTop: 30}}
+          source={{
+            uri: 'https://nid.naver.com/oauth2.0/authorize?client_id=U981wCCDuUbK6_3C3WJo&response_type=code&redirect_uri=http://localhost:8080/login/oauth2/code/naver', // 네이버 로그인 페이지 URL
+            headers: {
+              'Accept-Language': 'ko-KR,ko',
+            },
+          }}
+          method="GET"
+          headers={{
             'Accept-Language': 'ko-KR,ko',
-          },
-        }}
-        method="GET" // HTTP 메서드 설정
-        headers={{
-          'Accept-Language': 'ko-KR,ko',
-        }}
-        body={null} // HTTP 요청 본문 설정
-        javaScriptEnabled={true}
-        saveFormData={false}
-        onNavigationStateChange={navState => {
-          // URL 변경 시 콘솔에 로그 출력
-          console.log('URL 변경: ' + navState.url);
-
-          // URL을 parseAuthCode 함수에 전달
-          parseAuthCode(navState.url);
-        }}
-      />
+          }}
+          body={null}
+          javaScriptEnabled={true}
+          saveFormData={false}
+          onNavigationStateChange={navState => {
+            parseAuthCode(navState.url);
+          }}
+        />
+      )}
     </View>
   );
 };

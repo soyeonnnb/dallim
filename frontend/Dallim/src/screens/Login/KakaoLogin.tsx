374dfdd354c0a06d6b7ef98b2325ba49724fe67c
GyuhoChoi <s4248942@gmail.com>
2023-10-17 10:31:00 +0900

    Feat : 카카오 로그인 2차

--- frontend/Dallim/src/screens/Login/KakaoLogin.tsx
+++ frontend/Dallim/src/screens/Login/KakaoLogin.tsx
@@ -10,35 +10,34 @@ interface KakaoLoginProps {
 }
 
 const KakaoLogin = ({navigation}: KakaoLoginProps) => {
-  const sendAuthCodeToServer = async (authCode: any) => {
-    try {
-      const response = await axios.post(
-        'http://10.0.2.2:8080/login/oauth2/code/kakao',
-        null, // 요청 본문은 null로 설정합니다.
-        {
-          params: {
-            code: authCode,
-          },
-        },
-      );
-      console.log('서버 응답:', response.data);
-
-      // 서버 응답을 처리하고 필요한 동작을 수행할 수 있습니다.
-    } catch (error) {
-      console.error('서버 요청 실패:', error);
-    }
-  };
-
-  // parseAuthCode 함수 내에서 authCode를 얻은 후 호출
   const parseAuthCode = async (url: string) => {
     const exp = 'code='; //url에 붙어 날라오는 인가코드는 code=뒤부터 parse하여 get
     const startIndex = url.indexOf(exp); //url에서 "code="으로 시작하는 index를 찾지 못하면 -1반환
     if (startIndex !== -1) {
       const authCode = url.substring(startIndex + exp.length);
-      console.log('authCode ' + authCode);
+      console.log('access code :: ' + authCode);
 
-      // authCode를 서버로 전달
-      sendAuthCodeToServer(authCode);
+      console.log(
+        'Request URL: ',
+        'http://10.0.2.2:8080/api/oauth2/code/kakao',
+      );
+      console.log('Request Data: ', {params: {code: authCode}});
+
+      await axios
+        .post('http://10.0.2.2:8080/api/oauth2/code/kakao', null, {
+          params: {
+            code: authCode,
+          },
+        })
+        .then(res =>
+          AsyncStorage.setItem(
+            'userNumber',
+            JSON.stringify(res['data']['userId']),
+          ),
+        )
+        .catch(error => {
+          console.error('Axios Error: ', error);
+        });
 
       navigation.navigate('Home', {screen: 'Home'});
     }
@@ -52,6 +51,7 @@ const KakaoLogin = ({navigation}: KakaoLoginProps) => {
         style={{marginTop: 30}}
         source={{
           // uri: 'http://10.0.2.2:8080/login/oauth2/code/kakao',
+          // uri: 'https://kauth.kakao.com/oauth/authorize',
           // uri: 'https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=e9cb9f18c757bb2e5ec1c811a9fbe5d1&redirect_uri=http://10.0.2.2:8080/login/oauth2/code/kakao',
           uri: 'https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=e9cb9f18c757bb2e5ec1c811a9fbe5d1&redirect_uri=http://localhost:8080/login/oauth2/code/kakao',
           //client_id에는 본인 앱 등록후 발급받은 REST API KEY
@@ -62,8 +62,8 @@ const KakaoLogin = ({navigation}: KakaoLoginProps) => {
         }}
         javaScriptEnabled={true}
         // onMessage={event => {
-        //   console.log(event.nativeEvent['url']);
-        //   parseAuthCode(event.nativeEvent['url']);
+        //   console.log('찍' + event.nativeEvent.url);
+        //   // parseAuthCode(event.nativeEvent['url']);
         // }}
         onNavigationStateChange={navState => {
           // URL 변경 시 콘솔에 로그 출력

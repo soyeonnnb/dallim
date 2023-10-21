6a01e1458dbea06701fb2824990ed4881173467d
EUNSEONG LEE <pro30343069@gmail.com>
2023-10-21 17:49:34 +0900

    Feat: Chart 페이지 구성, 캐러셀 구현

---
+++ frontend/Dallim/src/components/chartComponent/SecondChart.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as S from './SecondChart.styles';
+
+function SecondChart() {
+
+    return (
+        <S.Container>
+            <S.Text> 두번째 차트 </S.Text>
+
+        </S.Container>
+    );
+}
+
+export default SecondChart;
--- frontend/Dallim/src/screens/chart/Chart.tsx
+++ frontend/Dallim/src/screens/chart/Chart.tsx
@@ -1,19 +1,48 @@
 import React from 'react';
-import { Button } from 'react-native';
 import * as S from './Chart.styles'; // 스타일 컴포넌트 import
+import Carousel from 'react-native-reanimated-carousel';
+import { Dimensions } from 'react-native';
+import Character from '../../assets/character/펭귄.png';
 
-interface ChartProps {
-    navigation: any; // navigation prop 타입은 실제 사용하는 라이브러리에 따라 다를 수 있습니다.
-}
+import FirstChart from '../../components/chartComponent/FirstChart';
+import SecondChart from '../../components/chartComponent/SecondChart';
+import ThirdChart from '../../components/chartComponent/ThirdChart';
 
-const Chart = ({ navigation }: ChartProps) => {
+function Chart() {
+    const { width } = Dimensions.get('window');
+
+    const Nickname = '하늘을 나는 펭소';
+    const components = [<FirstChart />, <SecondChart />, <ThirdChart />];
+
     return (
         <S.Container>
-            <S.Title>Chart Screen</S.Title>
-            <Button
-                title="Go back to Main"
-                onPress={() => navigation.navigate('Main')}
-            />
+            <S.BackgroundImage
+                source={require('../../assets/images/MainBackground3.png')}
+                resizeMode="cover">
+                <S.Body>
+                    <S.BodyBox>
+                        <S.Top>
+                            <S.CharacterBox>
+                                <S.Cycle>
+                                    <S.CharacterImage source={Character} />
+                                </S.Cycle>
+                            </S.CharacterBox>
+                            <S.NicknameBox>
+                                <S.NicknameText>{Nickname}</S.NicknameText>
+                            </S.NicknameBox>
+                        </S.Top>
+                        <S.Middle>
+                            <Carousel
+                                data={components}
+                                width={width / 3 * 2}
+                                renderItem={({ item, index }: { item: React.ReactElement; index: number }) =>
+                                    <React.Fragment key={index}>{item}</React.Fragment> // key prop 추가
+                                }
+                            />
+                        </S.Middle>
+                    </S.BodyBox>
+                </S.Body>
+            </S.BackgroundImage>
         </S.Container>
     );
 };

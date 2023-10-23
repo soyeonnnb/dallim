23ef8cf70a6fd160bf830574a61314414a273ab5
soyeonnnb <la28s5d@naver.com>
2023-10-23 22:23:04 +0900

    Feat: navbar finish

--- frontend/Dallim/src/components/common/bottomTab/BottomTab.tsx
+++ frontend/Dallim/src/components/common/bottomTab/BottomTab.tsx
@@ -3,10 +3,7 @@ import {
   BottomTabNavigationOptions,
   createBottomTabNavigator,
 } from '@react-navigation/bottom-tabs';
-// navigation
-import {NavigationContainer} from '@react-navigation/native';
-import * as varStyles from '../styles';
-import React, {useEffect, useReducer, useRef, useState} from 'react';
+import React, {useReducer, useState} from 'react';
 import {
   Pressable,
   StyleSheet,
@@ -14,17 +11,7 @@ import {
   Text,
   LayoutChangeEvent,
 } from 'react-native';
-import Svg, {Defs, Mask, Rect, Path, G} from 'react-native-svg';
-import Animated, {
-  useAnimatedStyle,
-  withTiming,
-  useDerivedValue,
-  useSharedValue,
-  useAnimatedProps,
-} from 'react-native-reanimated';
-import {Dimensions} from 'react-native';
-
-const {width, height} = Dimensions.get('window');
+import Animated, {useAnimatedStyle, withTiming} from 'react-native-reanimated';
 
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
@@ -39,9 +26,6 @@ import Profile from '../../../screens/profile/Profile';
 import BottomTabIcon from './BottomTabIcon';
 
 const Tab = createBottomTabNavigator();
-const AnimatedSvg = Animated.createAnimatedComponent(Svg);
-const AnimatedPath = Animated.createAnimatedComponent(Path);
-const AnimatedRect = Animated.createAnimatedComponent(Rect);
 
 const AnimatedTabBar = ({
   state: {index: activeIndex, routes},
@@ -49,33 +33,6 @@ const AnimatedTabBar = ({
   descriptors,
 }: BottomTabBarProps) => {
   const {bottom} = useSafeAreaInsets();
-  const animatedValue = useSharedValue(0);
-  const translateX = useSharedValue(-width); // 시작점을 화면 밖으로 설정
-  // SVG의 각 Rect에 대한 애니메이션 속성을 만듭니다.
-  const animatedPropsRed = useAnimatedProps(() => {
-    return {
-      x: translateX.value - width, // 초기 위치에서 시작
-    };
-  });
-
-  const animatedPropsBlue = useAnimatedProps(() => {
-    return {
-      x: translateX.value, // 두 번째 박스는 첫 번째 다음에 있습니다.
-    };
-  });
-
-  const animatedPropsGreen = useAnimatedProps(() => {
-    return {
-      x: translateX.value + width, // 세 번째 박스는 두 번째 다음에 있습니다.
-    };
-  });
-  useEffect(() => {
-    // 여기서 애니메이션 목표 값을 설정합니다. 예를 들어, 1까지.
-    animatedValue.value = withTiming(1, {duration: 3000}); // 3초 동안
-  }, []);
-
-  const AnimatedPath = Animated.createAnimatedComponent(Path);
-  const animatedPathOffset = useSharedValue(0);
 
   const reducer = (state: any, action: {x: number; index: number}) => {
     return [...state, {x: action.x, index: action.index}];
@@ -87,66 +44,16 @@ const AnimatedTabBar = ({
     dispatch({x: event.nativeEvent.layout.x, index});
   };
 
-  const xOffset = useDerivedValue(() => {
-    if (layout.length !== routes.length) return 0;
-    return [...layout].find(({index}) => index === activeIndex)!.x - 25;
-  }, [activeIndex, layout]);
-
-  const animatedPathProps = useAnimatedProps(() => {
-    // xOffset 값에 기반하여 "d" 속성을 동적으로 변경합니다.
-    const d =
-      'M20 0H0c11.046 0 20 8.953 20 20v5c0 19.33 15.67 35 35 35s35-15.67 35-35v-5c0-11.045 8.954-20 20-20H20z';
-
-    return {d}; // d 속성을 업데이트하여 경로 변경
-  });
-  const animatedStyles = useAnimatedStyle(() => {
-    return {
-      transform: [{translateX: withTiming(xOffset.value, {duration: 250})}],
-    };
-  });
-
   return (
     <View
+      id="mainContainer"
       style={[
         {
-          position: 'absolute',
-          bottom: 0,
-          alignItems: 'center',
+          paddingBottom: bottom,
+          backgroundColor: 'white',
         },
       ]}>
-      <AnimatedSvg
-        width={width * 3}
-        height={60}
-        y="10"
-        viewBox={`${-width} 0 ${width * 3} 60`}
-        style={[styles.activeBackground, animatedStyles]}>
-        <Defs>
-          <Mask id="myMask" x="0" y="0" height="100%" width="100%">
-            {/* 주목: 여기서 width를 200%로 설정하여 마스크 영역을 넓혔습니다. */}
-            {/* 마스크 내부의 Rect 또는 Path 위치를 조정하려면 다음과 같이 x 속성을 변경합니다. */}
-            <Rect x="0" y="0" height="100%" width="100%" fill="white" />
-            {/* 이 Rect는 마스크의 범위를 정의합니다. */}
-            <AnimatedPath
-              x="-1"
-              animatedProps={animatedPathProps}
-              fill="#000"
-            />
-            {/* 여기서 'd'는 실제 경로 데이터입니다. 필요한 형태와 위치에 따라 이 값을 조정해야 합니다. */}
-          </Mask>
-        </Defs>
-        <Rect x={-width} y="0" width={width} height="100%" fill="white" />
-        <Rect
-          x="0"
-          y="0"
-          width={width}
-          height="100%"
-          fill="white"
-          mask="url(#myMask)" // 이 Rect (파란색)에 마스크가 적용됩니다.
-        />
-        <Rect x={width} y="0" width={width} height="100%" fill="white" />
-      </AnimatedSvg>
-
-      <View style={styles.tabBarContainer}>
+      <View style={[styles.tabBarContainer]} id="redContainer">
         {routes.map((route, index) => {
           const active = index === activeIndex;
           const {options} = descriptors[route.key];
@@ -179,15 +86,6 @@ const TabBarComponent = ({
   onLayout,
   onPress,
 }: TabBarComponentProps) => {
-  const ref = useRef(null);
-
-  useEffect(() => {
-    if (active && ref?.current) {
-      // @ts-ignore
-      ref.current.play();
-    }
-  }, [active]);
-
   const animatedComponentCircleStyles = useAnimatedStyle(() => {
     return {
       transform: [
@@ -195,7 +93,7 @@ const TabBarComponent = ({
           scale: withTiming(active ? 1 : 0, {duration: 250}),
         },
         {
-          translateY: withTiming(active ? -10 : 0, {duration: 200}),
+          translateY: withTiming(active ? -15 : 0, {duration: 200}),
         },
       ],
     };
@@ -205,10 +103,10 @@ const TabBarComponent = ({
     return {
       transform: [
         {
-          translateY: withTiming(active ? -10 : 0, {duration: 200}),
+          translateY: withTiming(active ? -15 : 0, {duration: 200}),
         },
       ],
-      opacity: withTiming(active ? 1 : 0.5, {duration: 250}),
+      opacity: withTiming(active ? 1 : 0.5, {duration: 200}),
     };
   });
 
@@ -219,8 +117,15 @@ const TabBarComponent = ({
       />
       <Animated.View
         style={[styles.iconContainer, animatedIconContainerStyles]}>
-        {/* @ts-ignore */}
-        {options.tabBarIcon ? options.tabBarIcon({ref}) : <Text>?</Text>}
+        {options.tabBarIcon && typeof options.tabBarIcon === 'function' ? (
+          options.tabBarIcon({
+            focused: active ? active : false,
+            color: '#000',
+            size: 25,
+          })
+        ) : (
+          <Text>No Icon</Text>
+        )}
       </Animated.View>
     </Pressable>
   );
@@ -229,15 +134,19 @@ const TabBarComponent = ({
 const styles = StyleSheet.create({
   activeBackground: {
     position: 'absolute',
+    bottom: 0,
   },
   tabBarContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    borderTopEndRadius: 30,
-    borderTopStartRadius: 30,
-    flex: 1,
     width: '100%',
-    zIndex: 1 - 1,
+    justifyContent: 'space-evenly',
+    backgroundColor: 'white',
+    borderTopEndRadius: 45,
+    borderTopStartRadius: 45,
+    zIndex: 2,
+    position: 'absolute',
+    bottom: 0,
+    flex: 1,
   },
   component: {
     height: 60,
@@ -246,7 +155,7 @@ const styles = StyleSheet.create({
   componentCircle: {
     flex: 1,
     borderRadius: 30,
-    backgroundColor: varStyles.colors.darkBlue,
+    backgroundColor: 'black',
   },
   iconContainer: {
     position: 'absolute',
@@ -263,7 +172,7 @@ const styles = StyleSheet.create({
   },
 });
 
-const App = () => {
+function BottompTab() {
   const [darkMode, setDarkMode] = useState(false);
   return (
     <Tab.Navigator
@@ -325,6 +234,6 @@ const App = () => {
       />
     </Tab.Navigator>
   );
-};
+}
 
-export default App;
+export default BottompTab;

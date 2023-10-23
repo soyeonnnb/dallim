1e9735b2d78675f30c488d01545b88f861295a53
EUNSEONG LEE <pro30343069@gmail.com>
2023-10-23 22:38:18 +0900

    Fix: edit screen

--- frontend/Dallim/src/screens/edit/Edit.styles.ts
+++ frontend/Dallim/src/screens/edit/Edit.styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import {ImageBackground} from 'react-native';
 
 export const Container = styled.View`
   flex: 1;
@@ -6,7 +7,39 @@ export const Container = styled.View`
   align-items: center;
 `;
 
-export const Title = styled.Text`
-  font-size: 24px;
-  margin-bottom: 20px;
+export const BackgroundImage = styled(ImageBackground)`
+  flex: 1;
+  width: 100%;
+  height: 100%;
+`;
+
+export const Top = styled.View`
+  border-width: 1px;
+  border-color: red;
+  flex-direction: row;
+  width: 100%;
+  height: 25%;
+`;
+
+export const Middle = styled.View`
+  border-width: 1px;
+  border-color: blue;
+  flex-direction: row;
+  width: 100%;
+  height: 55%;
+`;
+
+export const Bottom = styled.View`
+  border-width: 1px;
+  border-color: red;
+  flex-direction: row;
+  width: 100%;
+  height: 10%;
+`;
+
+export const TabBox = styled.View`
+  border-width: 1px;
+  border-color: red;
+  width: 100%;
+  height: 10%;
 `;
--- frontend/Dallim/src/screens/edit/Edit.tsx
+++ frontend/Dallim/src/screens/edit/Edit.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import { Button } from 'react-native';
 import * as S from './Edit.styles'; // 스타일 컴포넌트 import
 
-interface EditProps {
-    navigation: any; // navigation prop 타입은 실제 사용하는 라이브러리에 따라 다를 수 있습니다.
-}
-
-const Edit = ({ navigation }: EditProps) => {
+function Edit() {
     return (
         <S.Container>
-            <S.Title>Edit Screen</S.Title>
-            <Button
-                title="Go back to Main"
-                onPress={() => navigation.navigate('Main')}
-            />
+            <S.BackgroundImage
+                source={require('../../assets/images/MainBackground3.png')}
+                resizeMode="cover">
+                <S.Top>
+                </S.Top>
+                <S.Middle>
+                </S.Middle>
+                <S.Bottom>
+                </S.Bottom>
+                <S.TabBox />
+            </S.BackgroundImage>
         </S.Container>
     );
 };

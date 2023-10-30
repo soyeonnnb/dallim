ca579a51ca4fcab6cb575a0b1f2284326e1ba62e
soyeonnnb <la28s5d@naver.com>
2023-10-30 10:10:37 +0900

    Temp

--- frontend/Dallim/src/components/chartComponent/main/Preview.tsx
+++ frontend/Dallim/src/components/chartComponent/main/Preview.tsx
@@ -1,21 +1,42 @@
+/* eslint-disable curly */
 import React from 'react';
-import {useState} from 'react';
-
-import * as S from './Preview.styles';
-import Weekly from './PreviewWeekly';
-import Monthly from './PreviewMonthly';
-
-function CalendarPreview() {
-  const [isScrolled, setIsScrolled] = useState(false);
-  const handleScroll = () => {
-    console.log('scrolled');
-  };
+import {SafeAreaView, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import ActionSheet, {SheetManager} from 'react-native-actions-sheet';
+import {View} from 'react-native';
+const ExampleScreen = () => {
   return (
-    <S.Container>
-      {!isScrolled && <Weekly />}
-      {isScrolled && <Monthly />}
-    </S.Container>
+    <ActionSheet>
+      <View>
+        <Text>dsf</Text>
+      </View>
+    </ActionSheet>
   );
-}
+};
 
-export default CalendarPreview;
+export default ExampleScreen;
+
+const styles = StyleSheet.create({
+  btn: {
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    paddingHorizontal: 10,
+    borderRadius: 0,
+    width: '100%',
+    borderWidth: 2,
+    borderColor: 'white',
+  },
+  safeareview: {
+    flex: 1,
+    backgroundColor: '#e63946',
+    padding: 12,
+    paddingVertical: 20,
+    alignItems: 'center',
+    marginHorizontal: 12,
+  },
+  btnTitle: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
--- frontend/Dallim/src/screens/chart/ChartMain.tsx
+++ frontend/Dallim/src/screens/chart/ChartMain.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, {useState, useRef, createRef} from 'react';
 import * as S from './ChartMain.styles'; // 스타일 컴포넌트 import
-import {Dimensions} from 'react-native';
-import ChartCalendar from '../../components/chartComponent/main/Calendar';
-import CalendarPreview from '../../components/chartComponent/main/Preview';
+import ActionSheet, {
+  SheetProvider,
+  ActionSheetRef,
+} from 'react-native-actions-sheet';
+
+import Calendar from '../../components/chartComponent/main/Calendar';
+import Preview from '../../components/chartComponent/main/Preview';
 
 function Chart() {
+  const ref = useRef<ActionSheetRef>(null);
+  ref.current?.show();
+
   return (
     <>
       <S.BackgroundImage
-        source={require('../../assets/images/MainBackground3.png')}
+        source={require('@/assets/images/MainBackground3.png')}
         resizeMode="cover"
       />
       <S.Container>
-        <ChartCalendar />
-        <CalendarPreview />
+        <Calendar />
+        <SheetProvider>
+          <Preview />
+        </SheetProvider>
       </S.Container>
     </>
   );

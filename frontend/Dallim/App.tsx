2ec8eaff0427b58095938bc4259def9808f4fb54
EUNSEONG LEE <pro30343069@gmail.com>
2023-11-07 16:42:08 +0900

    Feat: 배경음악

--- frontend/Dallim/App.tsx
+++ frontend/Dallim/App.tsx
@@ -1,18 +1,17 @@
 import * as React from 'react';
 
-import {useEffect} from 'react';
-import {NavigationContainer, useNavigation} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import {RecoilRoot} from 'recoil';
+import { useEffect } from 'react';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { RecoilRoot } from 'recoil';
 import BottomTab from './src/components/common/bottomTab/BottomTab';
 import Kakao from './src/screens/login/KakaoLogin';
 import Login from './src/screens/login/Login';
 import NotFound from './src/screens/notFound/NotFound';
 import Naver from './src/screens/login/NaverLogin';
 import Toast from 'react-native-toast-message';
-import {enableScreens} from 'react-native-screens';
-import {DeviceEventEmitter} from 'react-native';
-import {Alert} from 'react-native';
+import { enableScreens } from 'react-native-screens';
+import { DeviceEventEmitter } from 'react-native';
 import messaging from '@react-native-firebase/messaging';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {
@@ -23,8 +22,9 @@ import {
   checkNotifications,
   requestNotifications,
 } from 'react-native-permissions';
-import {Platform, Linking} from 'react-native';
-import {displayNoti} from './src/utils/pushnotification_helper';
+import { Platform, Linking } from 'react-native';
+import { displayNoti } from './src/utils/pushnotification_helper';
+import Sound from 'react-native-sound';
 
 enableScreens();
 const Stack = createStackNavigator();
@@ -56,7 +56,29 @@ function NavigationWithListener() {
 
   return null; // 이 컴포넌트는 UI를 렌더링하지 않습니다.
 }
+
 function App() {
+  // 배경쏭
+  useEffect(() => {
+    Sound.setCategory('Playback'); // 배경음악 재생 설정
+    const bgm = new Sound('samplebgm.wav', Sound.MAIN_BUNDLE, (error) => {
+      if (error) {
+        console.log('오디오 로드 실패:', error);
+        return;
+      }
+      bgm.setNumberOfLoops(-1); // 무한 반복
+      bgm.play((success) => {
+        if (!success) {
+          console.log('오디오 재생 실패:');
+        }
+      });
+    });
+
+    return () => {
+      bgm.release(); // 앱 종료 시 오디오 리소스 해제
+    };
+  }, []);
+
   useEffect(() => {
     requestUserPermission();
     NotificationListner();
@@ -69,17 +91,15 @@ function App() {
 
   useEffect(() => {
     const requestNotificationPermission = async () => {
-      const {status} = await checkNotifications();
+      const { status } = await checkNotifications();
 
       if (status === 'denied') {
-        requestNotifications(['alert', 'sound']).then(({status}) => {
-          // If the status is still denied, guide the user to the settings
+        requestNotifications(['alert', 'sound']).then(({ status }) => {
           if (
             status === 'denied' &&
             Platform.OS === 'android' &&
             Platform.Version >= 33
           ) {
-            // Open the app's settings page in the system settings
             Linking.openSettings();
           }
         });
@@ -116,27 +136,27 @@ function App() {
           <Stack.Screen
             name="BottomTab"
             component={BottomTab}
-            options={{headerShown: false}} // BottomTab의 헤더 숨기기
+            options={{ headerShown: false }} // BottomTab의 헤더 숨기기
           />
           <Stack.Screen
             name="Login"
             component={Login}
-            options={{headerShown: false}}
+            options={{ headerShown: false }}
           />
           <Stack.Screen
             name="NotFound"
             component={NotFound}
-            options={{headerShown: false}}
+            options={{ headerShown: false }}
           />
           <Stack.Screen
             name="Kakao"
             component={Kakao}
-            options={{headerShown: false}}
+            options={{ headerShown: false }}
           />
           <Stack.Screen
             name="Naver"
             component={Naver}
-            options={{headerShown: false}}
+            options={{ headerShown: false }}
           />
         </Stack.Navigator>
 

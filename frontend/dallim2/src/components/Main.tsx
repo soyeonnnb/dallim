d20cb65cf9668557349dcb7345cc4af7b71bdbf8
soyeonnnb <la28s5d@naver.com>
2023-11-09 22:08:16 +0900

    Docs: 초기 페이지 생성

---
+++ frontend/dallim2/src/components/Main.tsx
@@ -0,0 +1,6 @@
+import * as S from "./Main.styles";
+
+function Main() {
+  return <S.Container>Main</S.Container>;
+}
+export default Main;
---
+++ frontend/dallim2/src/components/Theme.tsx
@@ -0,0 +1,6 @@
+import * as S from "./Theme.styles";
+
+function Theme() {
+  return <S.Container>Themes</S.Container>;
+}
+export default Theme;

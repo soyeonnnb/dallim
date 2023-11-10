18f9ff1a0462944b3545f076f4fd16d9bb283682
GyuhoChoi <s4248942@gmail.com>
2023-11-10 11:15:24 +0900

    Feat : 자동로그인 구현

---
+++ frontend/Dallim/src/screens/Login/AccessToken.tsx
@@ -0,0 +1,32 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as S from './Login.styles';
+import React, {useEffect} from 'react';
+import {Text} from 'react-native-svg';
+import {CommonActions, useNavigation} from '@react-navigation/native';
+
+interface Props {
+  navigation: any;
+}
+
+const AccessToken = ({navigation}: Props) => {
+  useEffect(() => {
+    const checkTokenAndNavigate = async () => {
+      const token = await AsyncStorage.getItem('accessToken');
+      console.log('[access Token]' + token);
+      if (token) {
+        navigation.navigate('BottomTab', {
+          screen: 'Main',
+        });
+      } else {
+        // token이 없으면 Login 화면으로 navigate합니다.
+        navigation.replace('Login');
+      }
+    };
+
+    checkTokenAndNavigate();
+  }, []);
+
+  return <S.Container></S.Container>;
+};
+
+export default AccessToken;

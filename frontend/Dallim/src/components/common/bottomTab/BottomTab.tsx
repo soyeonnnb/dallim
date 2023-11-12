4c4bd0d2fbaa394321b11d8a6c4d104c60179a2c
GyuhoChoi <s4248942@gmail.com>
2023-11-12 23:40:10 +0900

    Feat : 랭킹페이지 헤더 완성

--- frontend/Dallim/src/components/common/bottomTab/BottomTab.tsx
+++ frontend/Dallim/src/components/common/bottomTab/BottomTab.tsx
@@ -3,9 +3,9 @@ import {
   BottomTabNavigationOptions,
   createBottomTabNavigator,
 } from '@react-navigation/bottom-tabs';
-import React, { useReducer } from 'react';
-import { StyleSheet, View, Text, LayoutChangeEvent } from 'react-native';
-import Animated, { useAnimatedStyle, withTiming } from 'react-native-reanimated';
+import React, {useReducer} from 'react';
+import {StyleSheet, View, Text, LayoutChangeEvent} from 'react-native';
+import Animated, {useAnimatedStyle, withTiming} from 'react-native-reanimated';
 import * as S from './BottomTab.styles';
 
 // components
@@ -17,7 +17,7 @@ import Edit from '@/screens/edit/Edit';
 
 // icon
 import BottomTabIcon from './BottomTabIcon';
-import BottomBarIcon from '@/assets/icons/BottomBarIcon.svg'
+import BottomBarIcon from '@/assets/icons/BottomBarIcon.svg';
 
 // stackNavigator
 import MainStackNavigators from '../../../navigations/MainStackNavigators';
@@ -28,25 +28,25 @@ import SocialStackNavigators from '../../../navigations/SocialStackNavigators';
 const Tab = createBottomTabNavigator();
 
 const AnimatedTabBar = ({
-  state: { index: activeIndex, routes },
+  state: {index: activeIndex, routes},
   navigation,
   descriptors,
 }: BottomTabBarProps) => {
-  const reducer = (state: any, action: { x: number; index: number }) => {
-    return [...state, { x: action.x, index: action.index }];
+  const reducer = (state: any, action: {x: number; index: number}) => {
+    return [...state, {x: action.x, index: action.index}];
   };
 
   const [layout, dispatch] = useReducer(reducer, []);
 
   const handleLayout = (event: LayoutChangeEvent, index: number) => {
-    dispatch({ x: event.nativeEvent.layout.x, index });
+    dispatch({x: event.nativeEvent.layout.x, index});
   };
 
   return (
     <View style={animationStyles.tabBarContainer}>
       {routes.map((route, index) => {
         const active = index === activeIndex;
-        const { options } = descriptors[route.key];
+        const {options} = descriptors[route.key];
 
         return (
           <TabBarComponent
@@ -69,15 +69,15 @@ type TabBarComponentProps = {
   onPress: () => void;
 };
 
-const TabBarComponent = ({ active, options, onPress }: TabBarComponentProps) => {
+const TabBarComponent = ({active, options, onPress}: TabBarComponentProps) => {
   const animatedComponentCircleStyles = useAnimatedStyle(() => {
     return {
       transform: [
         {
-          scale: withTiming(active ? 1 : 0, { duration: 200 }),
+          scale: withTiming(active ? 1 : 0, {duration: 200}),
         },
         {
-          translateY: withTiming(active ? -25 : 0, { duration: 200 }),
+          translateY: withTiming(active ? -25 : 0, {duration: 200}),
         },
       ],
     };
@@ -88,10 +88,10 @@ const TabBarComponent = ({ active, options, onPress }: TabBarComponentProps) => 
     return {
       transform: [
         {
-          translateY: withTiming(active ? -35 : 0, { duration: 200 }),
+          translateY: withTiming(active ? -35 : 0, {duration: 200}),
         },
       ],
-      opacity: withTiming(active ? 1 : 0.5, { duration: 200 }),
+      opacity: withTiming(active ? 1 : 0.5, {duration: 200}),
     };
   });
 
@@ -103,13 +103,15 @@ const TabBarComponent = ({ active, options, onPress }: TabBarComponentProps) => 
       <S.PressableContainer onPress={onPress}>
         <Animated.View
           style={[animationStyles.iconContainer, animatedIconContainerStyles]}>
-
           {active && (
-            <BottomBarIcon width={80} height={80}
+            <BottomBarIcon
+              width={80}
+              height={80}
+              color="#EBECF0"
               // style={StyleSheet.absoluteFill}
               style={{
                 ...StyleSheet.absoluteFillObject,
-                transform: [{ translateY: -5 }]
+                transform: [{translateY: -5}],
               }}
             />
           )}
@@ -133,13 +135,13 @@ function BottompTab() {
   return (
     <Tab.Navigator
       tabBar={props => <AnimatedTabBar {...props} />}
-      screenOptions={{ headerShown: false }}
+      screenOptions={{headerShown: false}}
       initialRouteName="Main">
       <Tab.Screen
         name="Chart"
         component={ChartStackNavigators}
         options={{
-          tabBarIcon: ({ focused }) => (
+          tabBarIcon: ({focused}) => (
             <BottomTabIcon focused={focused} type="chart" />
           ),
         }}
@@ -148,7 +150,7 @@ function BottompTab() {
         name="SocialStack"
         component={SocialStackNavigators}
         options={{
-          tabBarIcon: ({ focused }) => (
+          tabBarIcon: ({focused}) => (
             <BottomTabIcon focused={focused} type="social" />
           ),
         }}
@@ -157,7 +159,7 @@ function BottompTab() {
         name="Main"
         component={MainStackNavigators}
         options={{
-          tabBarIcon: ({ focused }) => (
+          tabBarIcon: ({focused}) => (
             <BottomTabIcon focused={focused} type="main" />
           ),
         }}
@@ -166,7 +168,7 @@ function BottompTab() {
         name="Edit"
         component={Edit}
         options={{
-          tabBarIcon: ({ focused }) => (
+          tabBarIcon: ({focused}) => (
             <BottomTabIcon focused={focused} type="edit" />
           ),
         }}
@@ -175,7 +177,7 @@ function BottompTab() {
         name="ProfileStack"
         component={ProfileStackNavigators}
         options={{
-          tabBarIcon: ({ focused }) => (
+          tabBarIcon: ({focused}) => (
             <BottomTabIcon focused={focused} type="profile" />
           ),
           unmountOnBlur: true,
@@ -189,8 +191,9 @@ const animationStyles = StyleSheet.create({
   tabBarContainer: {
     flexDirection: 'row',
     width: '100%',
+    // height: '100px',
     justifyContent: 'space-evenly',
-    backgroundColor: 'rgba(21, 28, 72, 0.5)',
+    backgroundColor: 'rgba(59, 54, 112, 0.8)',
     // borderTopEndRadius: 45,
     // borderTopStartRadius: 45,
     zIndex: 2,

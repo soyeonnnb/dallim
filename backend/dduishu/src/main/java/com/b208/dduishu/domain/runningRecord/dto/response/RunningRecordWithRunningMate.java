d7fae7a660f42bab1c262fe2cd19bbd65853a5a9
pum005 <pum005@naver.com>
2023-11-14 10:13:11 +0900

    feat : 캐릭터 정보, rivalRecord distance 추가

--- backend/dduishu/src/main/java/com/b208/dduishu/domain/runningRecord/dto/response/RunningRecordWithRunningMate.java
+++ backend/dduishu/src/main/java/com/b208/dduishu/domain/runningRecord/dto/response/RunningRecordWithRunningMate.java
@@ -13,21 +13,27 @@ import static java.util.stream.Collectors.toList;
 public class RunningRecordWithRunningMate {
 
     private String id;
+    private int characterIndex;
+    private int evolutionStage;
     private WinOrLose winOrLose;
     private LocalDateTime createdAt;
     private List<Double> mySpeed;
     private List<Double> rivalSpeed;
     private int totalTime;
     private double totalDistance;
+    private double rivalTotalDistance;
     private double averageHeartRate;
 
     public RunningRecordWithRunningMate(RunningRecord runningRecord, RunningRecord rivalRecord) {
         this.id = runningRecord.getId().toString();
+        this.characterIndex = runningRecord.getCharacter().getCharacterIndex();
+        this.evolutionStage = runningRecord.getCharacter().getEvolutionStage();
         this.winOrLose = runningRecord.getWinOrLose();
         this.mySpeed = getSpeed(runningRecord);
         this.rivalSpeed = getSpeed(rivalRecord);
         this.totalTime = runningRecord.getTotalTime();
         this.totalDistance = runningRecord.getTotalDistance();
+        this.rivalTotalDistance = rivalRecord.getTotalDistance();
         this.averageHeartRate = runningRecord.getAverageHeartRate();
         this.createdAt = runningRecord.getCreatedAt();
     }

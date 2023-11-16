8a45d408911d9b9bd1b325ff75b0aff0ffc39839
pum005 <pum005@naver.com>
2023-11-16 10:04:03 +0900

    refactor : Level 로직 리팩토링

--- backend/dduishu/src/main/java/com/b208/dduishu/domain/user/entity/BaseLevel.java
+++ backend/dduishu/src/main/java/com/b208/dduishu/domain/user/entity/BaseLevel.java
@@ -3,43 +3,46 @@ package com.b208.dduishu.domain.user.entity;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class BaseLevel {
+
+    private static int LEVEL_GAGE = 30000;
+
     @Data
     static class Level {
+        private int level;
         private int startExp;
         private int endExp;
 
         @Builder
-        public Level(int startExp, int endExp) {
+        public Level(int level, int startExp, int endExp) {
+            this.level = level;
             this.startExp = startExp;
             this.endExp = endExp;
         }
     }
 
-    private static final List<Level> baseLevels = List.of(
-            Level.builder().startExp(0).endExp(500).build(),
-            Level.builder().startExp(500).endExp(1500).build(),
-            Level.builder().startExp(1500).endExp(3000).build(),
-            Level.builder().startExp(3000).endExp(5000).build(),
-            Level.builder().startExp(5000).endExp(7500).build(),
-            Level.builder().startExp(7500).endExp(10500).build(),
-            Level.builder().startExp(10500).endExp(14000).build(),
-            Level.builder().startExp(14000).endExp(18000).build(),
-            Level.builder().startExp(14000).endExp(18000).build(),
-            Level.builder().startExp(14000).endExp(18000).build(),
-            Level.builder().startExp(18000).endExp(22500).build(),
-            Level.builder().startExp(22500).endExp(27500).build(),
-            Level.builder().startExp(27500).endExp(33000).build()
-            );
+    public BaseLevel() {
+        createBaseLevels();
+    }
+
+    private void createBaseLevels() {
+        for ( int i = 0 ; i < 50 ; i++) {
+            baseLevels.add(Level.builder().level(i+1).startExp(LEVEL_GAGE * i).endExp(LEVEL_GAGE * (i+1)).build());
+        }
+    }
+
+    private static final List<Level> baseLevels = new ArrayList<>();
 
     public static LevelInfo getLevelInfo(int exp) {
         return baseLevels.stream()
                 .filter(o -> o.getStartExp() <= exp && exp < o.getEndExp())
                 .map(o -> {
                     LevelInfo levelInfo = new LevelInfo();
+                    levelInfo.setLevel(o.getLevel());
                     levelInfo.setCurExp(exp - o.getStartExp());
                     levelInfo.setEndExp(o.getEndExp());
                     levelInfo.setExp((int)(((double)(exp - o.getStartExp()) / (o.getEndExp() - o.getStartExp())) * 100));
@@ -51,6 +54,7 @@ public class BaseLevel {
 
     @Data
     public static class LevelInfo {
+        private int level;
         private int curExp;
         private int endExp;
         private int exp;
--- backend/dduishu/src/main/java/com/b208/dduishu/domain/user/entity/UserLevel.java
+++ backend/dduishu/src/main/java/com/b208/dduishu/domain/user/entity/UserLevel.java
@@ -11,6 +11,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 public class UserLevel {
 
+    private static int LEVEL_GAGE = 30000;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -33,91 +34,9 @@ public class UserLevel {
     }
 
     private void setUserLevel(){
-        int e = this.exp;
+        BaseLevel.LevelInfo levelInfo = BaseLevel.getLevelInfo(exp);
 
-        int lv = 1;
-        if(e<500){
-            lv=1;
-        }else if(e<1500){
-            lv=2;
-        }else if(e<3000){
-            lv=3;
-        }else if(e<5000){
-            lv=4;
-        }else if(e<7500){
-            lv=5;
-        }else if(e<10500){
-            lv=6;
-        }else if(e<14000){
-            lv=7;
-        }else if(e<18000){
-            lv=8;
-        }else if(e<22500){
-            lv=9;
-        }else if(e<27500){
-            lv=10;
-        }else if(e<33000){
-            lv=11;
-        }else if(e<39000){
-            lv=12;
-        }else if(e<45500){
-            lv=13;
-        }else if(e<52500){
-            lv=14;
-        }else if(e<60000){
-            lv=15;
-        }else if(e<68000){
-            lv=16;
-        }else if(e<76500){
-            lv=17;
-        }else if(e<85500){
-            lv=18;
-        }else if(e<95000){
-            lv=19;
-        }else if(e<105000){
-            lv=20;
-        }else if(e<120000){
-            lv=21;
-        }else if(e<150000){
-            lv=22;
-        }else if(e<200000){
-            lv=23;
-        }else if(e<250000){
-            lv=24;
-        }else if(e<300000){
-            lv=25;
-        }else if(e<400000){
-            lv=26;
-        }else if(e<450000){
-            lv=27;
-        }else if(e<500000){
-            lv=28;
-        }else if(e<550000){
-            lv=29;
-        }else if(e<600000){
-            lv=30;
-        }else if(e<650000){
-            lv=31;
-        }else if(e<700000){
-            lv=32;
-        }else if(e<750000){
-            lv=33;
-        }else if(e<800000){
-            lv=34;
-        }else if(e<850000){
-            lv=35;
-        }else if(e<900000){
-            lv=36;
-        }else if(e<950000){
-            lv=37;
-        }else if(e<1000000){
-            lv=38;
-        }else{
-            lv=39;
-        }
-
-        this.level = lv;
-
+        this.level = levelInfo.getLevel();
     }
 
 
